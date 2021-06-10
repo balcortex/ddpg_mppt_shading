3dc6c1d11eb40d898959ddd259865da0b5314bbc
Bal Cortex <balcortex@hotmail.com>
2021-06-10 12:14:30 -0500

    clean pvsys

--- src/pvsys.py
+++ src/pvsys.py
@@ -18,7 +18,6 @@ SIMULINK_MODEL_NAME = "pv_boost_avg_rload"
 SIM_PARAMS = {
     "irradiance": [1000, 400, 300, 1000],
     "ambient_temperature": [40, 40, 40, 40],
-    "capacitor_initial_voltage": 0,
     "dc_bus_voltage": 48,
     "rload_resistance": 80,
     "duty_cycle": 0,
@@ -82,7 +81,6 @@ class ShadedArray:
             sim_params = {
                 "irradiance": [100, 400, 300, 200],
                 "ambient_temperature": [40, 40, 40, 40],
-                "capacitor_initial_voltage": 0,
                 "dc_bus_voltage": 48,
                 "rload_resistance": 80,
                 "duty_cycle": 0,
@@ -100,8 +98,8 @@ class ShadedArray:
         decimals: int = DECIMALS,
     ):
         self.mh = mh
-        self.decimals = decimals
-        # Get this variables from MATLAB's workspace after ruuning the simulink model
+        self._decimals = decimals
+        # Get this variables from MATLAB's workspace after running the simulink model
         self._vars = ("I_PV", "V_PV", "V_MOD1", "V_MOD2", "V_MOD3", "V_MOD4", "duty")
         # Variables to compose the key
         self._keys = ("irradiance", "ambient_temperature", "diode_state", "duty_cycle")
@@ -114,9 +112,10 @@ class ShadedArray:
         self.sim_params = dict()
         self.set_params(sim_params)
 
+        # Initialize cache files
         self.cache_path = matlab_source_path.joinpath(simulink_model_name + ".txt")
         self.cache_mpp_path = matlab_source_path.joinpath(
-            simulink_model_name + f"_mpp_{self.decimals}.txt"
+            simulink_model_name + f"_mpp_{self._decimals}.txt"
         )
         if not self.cache_path.exists():
             self.cache_path.touch()
@@ -154,13 +153,6 @@ class ShadedArray:
         command_template = "'pv_boost_avg_rload/Temperature {}', 'Value', '{}'"
         for idx, t in enumerate(self.cell_t, start=1):
             self.mh.eval_args("set_param", command_template.format(idx, t))
-
-    def set_capacitor_initial_voltage(self, voltage: numbers.Real) -> None:
-        """Set the capacitor initial voltage"""
-        self.sim_params.update({"capacitor_initial_voltage": voltage})
-        self.mh.eval_args(
-            "set_param", f"'pv_boost_avg_rload/C', 'InitialVoltage', '{voltage}'"
-        )
 
     def set_dc_bus_voltage(self, voltage: numbers.Real) -> None:
         """Set the PV system's DC bus voltage"""
@@ -214,7 +206,7 @@ class ShadedArray:
         """Specify the simulation parameters and return the result"""
         if duty_cycle is not None:
             duty_cycle = min(max(duty_cycle, 0), 1)  # clip between [0, 1]
-            duty_cycle = round(duty_cycle, self.decimals)
+            duty_cycle = round(duty_cycle, self._decimals)
             self.sim_params.update({"duty_cycle": duty_cycle})
         if irradiance:
             self.sim_params.update({"irradiance": tuple(irradiance)})
@@ -223,27 +215,16 @@ class ShadedArray:
 
         if self.is_cached:
             return SimulinkModelOutput.from_string(self.cache[self.key])
+        else:
+            self.set_params()
+            res = self.run()
+            self.cache[self.key] = str(res)
+            utils.save_dic_txt(self.cache, self.cache_path, overwrite=False)
 
-        self.set_params()
-        res = self.run()
-        self._add_to_cache(res)
-        self.save_cache_dic()
-
-        return res
-
-    def save_cache_dic(self, overwrite: bool = False) -> None:
-        """Save the cache dictionary as a txt file
-
-        Parameters:
-            - overwrite: If `True` overwrite the file, else append to the file
-
-        """
-        utils.save_dic_txt(self.cache, self.cache_path, overwrite=overwrite)
-        utils.save_dic_txt(self.cache_mpp, self.cache_mpp_path, overwrite=overwrite)
+            return res
 
     def get_shaded_iv_curve(
         self,
-        duty_cycle: Optional[numbers.Real] = None,
         irradiance: Optional[Sequence[numbers.Real]] = None,
         ambient_temperature: Optional[Sequence[numbers.Real]] = None,
         curve_points: int = 10 ** DECIMALS,
@@ -251,7 +232,7 @@ class ShadedArray:
     ) -> ShadedIVCurve:
         """Get the IV curve for the PV system and each of its modules"""
         # Update the parameters if given
-        self.simulate(duty_cycle, irradiance, ambient_temperature)
+        self.simulate(irradiance=irradiance, ambient_temperature=ambient_temperature)
 
         curves = list()
         for dc in tqdm(
@@ -265,7 +246,6 @@ class ShadedArray:
 
     def get_mpp(
         self,
-        duty_cycle: Optional[numbers.Real] = None,
         irradiance: Optional[Sequence[numbers.Real]] = None,
         ambient_temperature: Optional[Sequence[numbers.Real]] = None,
         curve_points: int = 10 ** DECIMALS,
@@ -273,28 +253,29 @@ class ShadedArray:
     ) -> SimulinkModelOutput:
         """Compute the MPP"""
         # Update the parameters if given
-        self.simulate(duty_cycle, irradiance, ambient_temperature)
+        self.simulate(irradiance=irradiance, ambient_temperature=ambient_temperature)
 
         if self.is_cached_mpp:
             return MPP.from_string(self.cache_mpp[self.key_mpp])
+        else:
+            curves = self.get_shaded_iv_curve(curve_points, verbose=verbose)
+            power = self.power(curves.current, curves.voltage)
+            idx = np.argmax(power)
 
-        curves = self.get_shaded_iv_curve(curve_points, verbose=verbose)
-        power = self.power(curves.current, curves.voltage)
-        idx = np.argmax(power)
+            mpp = MPP(
+                current=curves.current[idx],
+                voltage=curves.voltage[idx],
+                power=power[idx],
+                duty_cycle=curves.duty_cycle[idx],
+            )
 
-        mpp = MPP(
-            current=curves.current[idx],
-            voltage=curves.voltage[idx],
-            power=power[idx],
-            duty_cycle=curves.duty_cycle[idx],
-        )
+            self.cache_mpp[self.key_mpp] = str(mpp)
+            utils.save_dic_txt(self.cache_mpp, self.cache_mpp_path, overwrite=False)
 
-        self._add_to_cache_mpp(mpp)
-        self.save_cache_dic()
-
-        return mpp
+            return mpp
 
     def quit(self) -> None:
+        """Quit the MATLAB engine"""
         self.mh.quit()
 
     @property
@@ -436,14 +417,6 @@ class ShadedArray:
             simulink_model_name=SIMULINK_MODEL_NAME,
             sim_params=SIM_PARAMS,
         )
-
-    def _add_to_cache(self, output: SimulinkModelOutput) -> None:
-        """Add the simulation result to the cache dictionary"""
-        self.cache[self.key] = str(output)
-
-    def _add_to_cache_mpp(self, output: MPP) -> None:
-        """Add the simulation result to the cache MPP dictionary"""
-        self.cache_mpp[self.key_mpp] = str(output)
 
 
 if __name__ == "__main__":
